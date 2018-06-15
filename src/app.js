5574fca156a2d4c170d6d7c1f1acd62785a6db9b
Alberto <alberto.montiel@gmail.com>
2018-06-15 06:42:35 -0500

    Stateless components
    Class components converted to function components

--- src/app.js
+++ src/app.js
@@ -56,7 +56,16 @@ class IndecisionApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
+const Header = (props) =>{
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </div>
+    );
+}
+
+/* class Header extends React.Component {
     render() {
 
         return (
@@ -66,8 +75,19 @@ class Header extends React.Component {
             </div>
         );
     }
+} */
+
+const Action = (props) => {
+    return (
+        <div>
+            <button
+                onClick={props.handlePick}
+                disabled={!props.hasOptions}
+            >What should I do?</button>
+        </div>
+    );
 }
-class Action extends React.Component {
+/* class Action extends React.Component {
 
     render() {
         return (
@@ -79,8 +99,21 @@ class Action extends React.Component {
             </div>
         );
     }
+} */
+
+const Options = (props) =>{
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions}>Remove All</button>
+            <h3>Number of Options: {props.options.length}</h3>
+            {
+                props.options.map((option) => <Option key={option} optionText={option} />)
+            }
+            <p></p>
+        </div>
+    );
 }
-class Options extends React.Component {
+/* class Options extends React.Component {
 
     render() {
         return (
@@ -94,8 +127,16 @@ class Options extends React.Component {
             </div>
         );
     }
+} */
+
+const Option = (props) =>{
+    return (
+        <div>
+            {props.optionText}
+        </div>
+    );
 }
-class Option extends React.Component {
+/* class Option extends React.Component {
     render() {
         return (
             <div>
@@ -104,7 +145,7 @@ class Option extends React.Component {
         );
 
     }
-}
+} */
 class AddOption extends React.Component {
     constructor(props) {
         super(props);
@@ -134,4 +175,14 @@ class AddOption extends React.Component {
         );
     }
 }
+
+/* const User = (props) => {
+    return (
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+        </div>
+        );
+};
+ */
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
